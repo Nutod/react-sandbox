f5842a8996ac186b746f42fc31430758c45446f9
Nutod <alukodotun@gmail.com>
2022-04-18 14:52:32 +0200

    Initial commit

---
+++ cypress/cypress/integration/01-first-flight.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+// eslint-disable-next-line no-undef
+describe('Create a New Item', () => {
+  // it kinda like a headless browser thing. So Javascript works
+  // fine there
+  console.log('Running cy after a very long time');
+});
