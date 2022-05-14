685ec9b2c491cc168950aaec99c52c165f8e0c8e
Nutod <alukodotun@gmail.com>
2022-05-14 13:58:31 +0200

    feat: added new theme context provision logic

--- react-hooks-course-curriculum/app/contexts/theme.ts
+++ react-hooks-course-curriculum/app/contexts/theme.ts
@@ -1,6 +1,22 @@
 import React from 'react'
+import constate from 'constate'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+function useTheme() {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'dark' ? 'light' : 'dark'))
+  }, [])
+
+  return {
+    theme,
+    toggleTheme,
+  }
+}
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
