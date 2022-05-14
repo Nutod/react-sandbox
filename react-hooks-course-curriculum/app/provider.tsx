861d9e1fe32cfb2fd1e18f85eee2aeca1c3ea35f
Nutod <alukodotun@gmail.com>
2022-05-14 14:02:16 +0200

    feat: updated provider logic to use theme context

--- react-hooks-course-curriculum/app/provider.tsx
+++ react-hooks-course-curriculum/app/provider.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeContextProvider } from './contexts/theme'
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return <Router>{children}</Router>
+  return (
+    <Router>
+      <ThemeContextProvider>{children}</ThemeContextProvider>
+    </Router>
+  )
 }
