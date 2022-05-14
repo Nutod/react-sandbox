0cc83b7e93ac9597516a88dd91ff2fa8d8926195
Nutod <alukodotun@gmail.com>
2022-05-14 14:09:59 +0200

    feat: added typings to Comment component

--- react-hooks-course-curriculum/app/components/Comment.tsx
+++ react-hooks-course-curriculum/app/components/Comment.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import PostMetaInfo from './PostMetaInfo'
 
-export default function Comment ({ comment }) {
+export default function Comment({
+  comment,
+}: {
+  comment: {
+    by: string
+    time: number
+    id: number
+    text: string
+  }
+}) {
   return (
-    <div className='comment'>
+    <div className="comment">
       <PostMetaInfo
         comment={true}
         by={comment.by}
         time={comment.time}
         id={comment.id}
       />
-      <p dangerouslySetInnerHTML={{__html: comment.text}} />
+      <p dangerouslySetInnerHTML={{ __html: comment.text }} />
     </div>
   )
 }
