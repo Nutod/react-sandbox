6da485216987f1c1e69261c73b2df7b7db4add9e
Nutod <alukodotun@gmail.com>
2022-05-14 14:11:46 +0200

    feat: added typeings to PostMetaInfo component

--- react-hooks-course-curriculum/app/components/PostMetaInfo.tsx
+++ react-hooks-course-curriculum/app/components/PostMetaInfo.tsx
@@ -2,23 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { formatDate } from '../utils/helpers'
-import { ThemeConsumer } from '../contexts/theme'
+import { useThemeContext } from '../contexts/theme'
 
-export default function PostMetaInfo ({ by, time, id, descendants }) {
+type PostMetaInfoProps = {
+  by: string
+  time: number
+  id: number
+  descendants?: number
+  comment?: boolean
+}
+export default function PostMetaInfo({
+  by,
+  time,
+  id,
+  descendants,
+}: PostMetaInfoProps) {
+  const { theme } = useThemeContext()
+
   return (
-    <ThemeConsumer>
-      {({ theme }) => (
-        <div className={`meta-info-${theme}`}>
-          <span>by <Link to={`/user?id=${by}`}>{by}</Link></span>
-          <span>on {formatDate(time)}</span>
-          {typeof descendants === 'number' && (
-            <span>
-              with <Link to={`/post?id=${id}`}>{descendants}</Link> comments
-            </span>
-          )}
-        </div>
+    <div className={`meta-info-${theme}`}>
+      <span>
+        by <Link to={`/user?id=${by}`}>{by}</Link>
+      </span>
+      <span>on {formatDate(time)}</span>
+      {typeof descendants === 'number' && (
+        <span>
+          with <Link to={`/post?id=${id}`}>{descendants}</Link> comments
+        </span>
       )}
-    </ThemeConsumer>
+    </div>
   )
 }
 
