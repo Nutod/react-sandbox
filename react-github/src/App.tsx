e7e9d20860ee532d87e14ccf6e4b72d1e6a75622
Nutod <alukodotun@gmail.com>
2022-05-14 20:56:38 +0200

    feat: added new styling to App

--- react-github/src/App.tsx
+++ react-github/src/App.tsx
@@ -1,15 +1,29 @@
+import { Grid } from '@geist-ui/core'
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Nav from './components/Nav'
+import Popular from './components/Popular'
 
 function App() {
   // I'd be looking into a container component here
   return (
-    <div>
+    <div
+      style={{
+        maxWidth: '950px',
+        marginInline: 'auto',
+        background: 'pink',
+        marginBlockStart: '2rem',
+      }}
+    >
       <Nav />
 
       {/* Routing */}
-      <div>Routes</div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Popular />} />
+        </Routes>
+      </main>
     </div>
   )
 }
