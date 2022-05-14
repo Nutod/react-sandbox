de81620defa8d3aa159e4b08341397e98dc58f7a
Nutod <alukodotun@gmail.com>
2022-05-14 14:09:02 +0200

    feat: update Nav component

--- react-hooks-course-curriculum/app/components/Nav.tsx
+++ react-hooks-course-curriculum/app/components/Nav.tsx
@@ -1,44 +1,43 @@
 import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
+import { useThemeContext } from '../contexts/theme'
 import { NavLink } from 'react-router-dom'
 
-const activeStyle = {
-  color: 'rgb(187, 46, 31)'
-}
+export default function Nav() {
+  const { theme, toggleTheme } = useThemeContext()
 
-export default function Nav () {
   return (
-    <ThemeConsumer>
-      {({ theme, toggleTheme }) => (
-        <nav className='row space-between'>
-          <ul className='row nav'>
-            <li>
-              <NavLink
-                to='/'
-                exact
-                activeStyle={activeStyle}
-                className='nav-link'>
-                  Top
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/new'
-                activeStyle={activeStyle}
-                className='nav-link'>
-                  New
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            style={{fontSize: 30}}
-            className='btn-clear'
-            onClick={toggleTheme}
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => ({
+              color: isActive ? 'rgb(187, 46, 31)' : undefined,
+            })}
+            className="nav-link"
           >
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+            Top
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/new"
+            style={({ isActive }) => ({
+              color: isActive ? 'rgb(187, 46, 31)' : undefined,
+            })}
+            className="nav-link"
+          >
+            New
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? '🔦' : '💡'}
+      </button>
+    </nav>
   )
 }
