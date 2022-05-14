d83aed86fc92209a4922ac1eb82e8a3212685596
Nutod <alukodotun@gmail.com>
2022-05-14 20:47:43 +0200

    feat: added new styling provider

--- react-github/src/providers/Provider.tsx
+++ react-github/src/providers/Provider.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { GeistProvider, CssBaseline } from '@geist-ui/core'
 
 type ProviderProps = {
   children: React.ReactNode
 }
 
 export default function Provider({ children }: ProviderProps) {
-  return <Router>{children}</Router>
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Router>{children}</Router>
+    </GeistProvider>
+  )
 }
