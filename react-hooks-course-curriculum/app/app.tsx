a3d9b5767b9ce7f89899cfe7f91871a50d421d64
Nutod <alukodotun@gmail.com>
2022-05-14 14:03:42 +0200

    feat: added theme context to app component

--- react-hooks-course-curriculum/app/app.tsx
+++ react-hooks-course-curriculum/app/app.tsx
@@ -1,34 +1,35 @@
 import React from 'react'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './contexts/theme'
+import { Routes, Route } from 'react-router-dom'
 import Loading from './components/Loading'
 import Nav from './components/Nav'
+import Provider from './provider'
+import { useThemeContext } from './contexts/theme'
 
 const Posts = React.lazy(() => import('./components/Posts'))
 const Post = React.lazy(() => import('./components/Post'))
 const User = React.lazy(() => import('./components/User'))
 
 export function App() {
-  return (
-    <Router>
-      <ThemeProvider value={{ theme: 'light', toggleTheme: () => {} }}>
-        <div className={'light'}>
-          <div className="container">
-            <Nav />
+  const { theme } = useThemeContext()
 
-            <React.Suspense fallback={<Loading />}>
-              <Routes>
-                <Route path="/" element={<Posts type="top" />} />
-                <Route path="new" element={<Posts type="new" />} />
-                <Route path="post" element={<Post />} />
-                <Route path="user" element={<User />} />
-                <Route element={<h1>404</h1>} />
-              </Routes>
-            </React.Suspense>
-          </div>
+  return (
+    <Provider>
+      <div className={theme}>
+        <div className="container">
+          <Nav />
+
+          <React.Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Posts type="top" />} />
+              <Route path="new" element={<Posts type="new" />} />
+              <Route path="post" element={<Post />} />
+              <Route path="user" element={<User />} />
+              <Route element={<h1>404</h1>} />
+            </Routes>
+          </React.Suspense>
         </div>
-      </ThemeProvider>
-    </Router>
+      </div>
+    </Provider>
   )
 }
