a4d46998f5cb2f7dd8f9d1e5456ce26b22d3e466
Nutod <alukodotun@gmail.com>
2022-06-20 02:13:30 +0200

    fix: moved files and config around

--- react-github/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import VitePluginLinaria from 'vite-plugin-linaria'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh(), VitePluginLinaria()],
-})
