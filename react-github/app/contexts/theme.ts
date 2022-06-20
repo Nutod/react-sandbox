babe1988f30367e0cdcea7701bd04f21e1fcb9aa
Nutod <alukodotun@gmail.com>
2022-06-20 02:51:59 +0200

    feat: added new theme context logic

--- react-github/app/contexts/theme.ts
+++ react-github/app/contexts/theme.ts
@@ -1,6 +1,29 @@
 import React from 'react'
+import constate from 'constate'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+type ThemeContextProps = {
+  theme: 'light' | 'dark'
+  toggleTheme: () => void
+}
+
+const ThemeContext = React.createContext({} as ThemeContextProps)
+
+function useTheme() {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
+  }, [])
+
+  return {
+    theme,
+    toggleTheme,
+  }
+}
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
--- react-github/app/index.tsx
+++ react-github/app/index.tsx
@@ -1,49 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import { ThemeProvider } from './contexts/theme'
-import Nav from './components/Nav'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Loading from './components/Loading'
+import App from './app'
 
-const Popular = React.lazy(() => import('./components/Popular'))
-const Battle = React.lazy(() => import('./components/Battle'))
-const Results = React.lazy(() => import('./components/Results'))
-
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
-
-              <React.Suspense fallback={<Loading />} >
-                <Routes>
-                  <Route path='/' element={<Popular />} />
-                  <Route path='/battle' element={<Battle />} />
-                  <Route path='/battle/results' element={<Results />} />
-                  <Route element={() => <h1>404</h1>} />
-                </Routes>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'))
